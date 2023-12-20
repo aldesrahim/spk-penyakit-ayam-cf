c03b47d30c6925a7d53a3d1ac8619a2fb230fe8f
aldes <aldesrahim@gmail.com>
2023-12-20 23:35:01 +0700

    improve

--- resources/views/filament/widgets/refresh-database.blade.php
+++ resources/views/filament/widgets/refresh-database.blade.php
@@ -1,7 +1,7 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <x-filament::button size="lg" color="danger" wire:click="run">
-            Refresh Database (logout)
+            Refresh Database
         </x-filament::button>
     </x-filament::section>
 </x-filament-widgets::widget>
